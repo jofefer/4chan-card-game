5f5d612c877ae3982eb454340227da7664e787d8
jofefer <josefemeniaferrer@gmail.com>
2020-02-28 11:58:12 +0100

    some modifications

--- venv/src/main.py
+++ venv/src/main.py
@@ -2,35 +2,50 @@ import csv
 import random
 
 DEBUG = False
+if DEBUG:
+    print("............DEBUG MODE............")
+
 DIR_CARDS = "C:\\Users\\jfemeniafe001\\Documents\\Python Scripts\\github\\4chan-card-game\\venv\\csv\\listado_cartas.csv"
 
+#TODO: implement this class to extract all info
+# ocr url ---> https://ocr.space/OCRAPI
+class Ocr:
+    def convert_all(self):
+        pass
 
 class Card:
-    def __init__(self, id, type_card, name, description, effect_in_turn, debug = DEBUG):
+    #TODO add card IMG
+    def __init__(self, id, type_card, name, description, effect, debug = DEBUG):
         """ effect_in_turn:
-        0 - Pick someone
-        1 -
+        0 - to yourself
+        1 - pick someone
         """
         self.id = id
         self.type_card = type_card
         self.name = name
         self.description = description
-        self.effect_in_turn = effect_in_turn
+        self.effect = effect
         if debug:
             print("creada la carta, ", id)
 
     def __str__(self):
-        return 'Card({0},  {1}, "{2}", "{3}", {4})'.format(self.id, self.type_card, self.name, self.description, self.effect_in_turn)
+        return 'Card({0},  {1}, "{2}", "{3}", {4})'.format(self.id, self.type_card, self.name, self.description, self.effect)
+    
+    #TODO __desc(self)__
 
 
 class Deck:
-    def __init__(self, DIR_CARDS= DIR_CARDS):
+    def __init__(self, DIR_CARDS= DIR_CARDS, debug= DEBUG):
         self.deck = []
 
         with open(DIR_CARDS, 'r') as file:
             reader = csv.reader(file, delimiter='|')
             for row in reader:
                 self.deck.append(Card(*row))
+
+        if debug:
+            print("THE CARDS ARE:")
+            self.showCards()
 
     def __len__(self):
         return len(self.deck)
@@ -72,51 +87,56 @@ class Player:
 class Game:
     def __init__(self, playersList):
         self.deck = Deck()
-        self.players = [Player(*x) for x in playersList] #need to implement addPlayer
+        self.players = [Player(*x) for x in playersList]
 
+    # TODO implement addPlayer
     def addPlayer(self):
         pass
 
     def play(self):
-        turnos = 3
-        turno = 0
-        while turnos > 0:
-            if len(self.deck)== 0:
-                print("END, no more cards")
+        turns = 8
+        turn = 0
+        if turns == 0:
+            print("END OF THE GAME")
+        while turns > 0:
+            if len(self.deck) == 0:
+                print("GAME OVER    no more cards")
                 break
+
             print("-----------------------------------------")
-            player_tourn = turno % len(self.players)
+            player_tourn = turn % len(self.players)
             actual_player = self.players[player_tourn]
 
             new_card = self.deck.drawCard()
 
             print("{0} tourn ".format(actual_player.name))
-            print("the type of your card is {0} ".format( new_card.type_card))
             if int(new_card.type_card) == 0:
-                print(new_card.description)
+                print("ACTION!!! Do what it says:\n ----> \t",new_card.description)
+            elif int(new_card.type_card) == 1:
+                print("You have drow an INSTANT card")
+                actual_player.addCard(new_card)
+            elif int(new_card.type_card) == 2:
+                print("MANDATORY: \n ---->\t",new_card.description)
             else:
+                print("STATUS")
                 actual_player.addCard(new_card)
 
             menu = True
             while menu:
+                print("\nYour curent cards are:")
+                print(actual_player.showCards())
                 print("\nWhat do you want to do? \n"
-                      "\t1- Do you want to know your cards \n"
-                      "\t2- End your turn")
+                      "\t1- End your turn \n"
+                      "\t2- SOME OPTION")
                 option = int(input())
                 if option == 1:
-                    print("ha sido 1")
-                    print(actual_player.showCards())
-                if option == 2:
                     menu = False
                     continue
+            turns -= 1
+            turn += 1
 
 
 
-            turnos -= 1
-            turno += 1
-
-
-
-g1 = Game([["Pepe","male"],["Manoli","female"]])
+g1 = Game([["Pepe","male"],["Manoli","female"], ["player3", "transexual"]])
 g1.play()
 print("")
